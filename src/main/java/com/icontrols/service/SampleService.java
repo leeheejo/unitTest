7e9ab3896aa8f209d9d81836fafc38c9c579e81d
leeheejo <jo4610jo@naver.com>
2020-05-27 16:24:39 +0900

    first commit

---
+++ src/main/java/com/icontrols/service/SampleService.java
@@ -0,0 +1,12 @@
+package com.icontrols.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService {
+
+	
+	public String getName() {
+		return "heejo";
+	}
+}
